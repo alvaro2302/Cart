f6eeb9969744a2d6b759e89218b33b72dfb08fba
acuiza <alvaro230297@hotmail.com>
2023-08-20 20:51:37 -0400

    add method for update fee data

--- src/Context/index.js
+++ src/Context/index.js
@@ -1,4 +1,4 @@
-import { createContext,useState } from "react";
+import { createContext,useState, useEffect} from "react";
 import {lineItemss}from "../index";
 export const ShoppingCartContext = createContext()
 
@@ -11,6 +11,33 @@ export const ShoppingCartProvider = ({children}) =>{
     const [count, setCount] = useState(3)
     const [lineItems, setLineItems] = useState(lineItemss)
     const [isLoadingFee, setIsLoadingFee] = useState(false)
+
+    useEffect(()=>{
+        if(isLoadingFee) {
+            calculateFees()
+        }
+    },[isLoadingFee])
+    const calculateFees = () => {
+        let subtotal = 0;
+        let tax = 0;
+        let shipping = 0;
+        let total = 0;
+        lineItems.forEach((item)=>{
+            subtotal = subtotal + item.price;
+        })
+        tax = subtotal * 0.13;
+        shipping = 15;
+        total = subtotal + tax + shipping;
+        subtotal = Math.ceil(subtotal*100)/100;
+        tax = Math.ceil(tax*10000)/10000;
+        total = Math.ceil(total*10000)/10000;
+
+        setSubTotal(subtotal)
+        setHST(tax);
+        setShipping(shipping);
+        setTotal(total);
+        setIsLoadingFee(false);
+    }
     return (
         <ShoppingCartContext.Provider value ={{
             count,
