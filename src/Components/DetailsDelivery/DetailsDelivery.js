5639e70f4e1c6af05c7861274ec910baf39e38cd
acuiza <alvaro230297@hotmail.com>
2023-08-20 20:34:08 -0400

    add function for remove Item

--- src/Components/DetailsDelivery/DetailsDelivery.js
+++ src/Components/DetailsDelivery/DetailsDelivery.js
@@ -1,8 +1,20 @@
 import React,{useContext} from "react";
 import "./DetailsDelivery.css";
+import { ShoppingCartContext } from "../../Context";
 const DetailsDelivery = ({item}) => {
     const {price} = item
-
+    const context = useContext(ShoppingCartContext);
+    const {lineItems,setLineItems,setIsLoadingFee} = context;
+    const removeLineItem = (lineItemId) =>{
+        const newLineItems = lineItems.filter((lineItem)=>{
+            if (lineItem.id != lineItemId)
+            {
+                return item;
+            }
+        })
+        setLineItems(newLineItems);
+        setIsLoadingFee(true);
+    }
    
     
     return(
@@ -14,7 +26,7 @@ const DetailsDelivery = ({item}) => {
                     <h4>{"Estimated Delivery Date Dec 2 - Dec 15"}</h4>
                     
                   
-                    <button className="textDeliveryRemove" >{"Remove"}</button>
+                    <button onClick={() => removeLineItem(item.id)} className="textDeliveryRemove" >{"Remove"}</button>
                   
                 
                 </ul>    
